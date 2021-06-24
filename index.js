b973b07cd69fdf60503d3ad558b7cedd6540be44
Commie Walker <commie.walker@hotmail.com>
2021-06-24 10:36:31 -0700

    add routes for api

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express")
 const app = express();
 const db = require("./models");
+const routes = require("./routes")
+
+
 const PORT = process = process.env.PORT || 8080;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/",(req,res) => {
-    res.send("heyyy");
-})
+app.use(routes);
 
 db.sequelize.sync({force:true}).then(() => {
     app.listen(PORT, () => {
---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const router = require("express").Router();
+const db = require("../models")
+
+router.get("/all", (req,res) => {
+    db.Player.findAll().then(players => res.send(players));
+})
+
+router.get("/find/:id", (req,res) => {
+    db.Player.findAll({
+        where: {
+            id: req.params.id
+        }
+    }).then(player => res.send(player));
+})
+
+router.post("/add", (req,res) => {
+    db.Player.create({
+        name: req.body.name,
+        team: req.body.team,
+        age : req.body.age
+    });
+})
+
+
+
+router.get("/",(req,res) => {
+    res.send("heyyy u");
+})
+
+module.exports = router;
